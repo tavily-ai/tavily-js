8da5d291e7887f262c17cb8dcd3491e796976217
Konstantin Tzantchev <tzantchev@ucla.edu>
2024-09-27 16:22:58 -0700

    Improved typing and added image description options

--- src/search.ts
+++ src/search.ts
@@ -4,17 +4,18 @@ import { post } from "./utils";
 export function _search(apiKey: string) {
   return async function search(
     query: string,
-    options: SearchOptions = { 
+    options: SearchOptions = {
       searchDepth: "basic",
       topic: "general",
       days: 3,
       maxResults: 5,
       includeImages: false,
+      includeImageDescriptions: false,
       includeAnswer: false,
       includeRawContent: false,
       includeDomains: undefined,
-      excludeDomains: undefined
-     }
+      excludeDomains: undefined,
+    }
   ) {
     const response = await post("search", {
       api_key: apiKey,
@@ -24,12 +25,33 @@ export function _search(apiKey: string) {
       days: options.days,
       max_results: options.maxResults,
       include_images: options.includeImages,
+      include_image_descriptions: options.includeImageDescriptions,
       include_answer: options.includeAnswer,
       include_raw_content: options.includeRawContent,
       include_domains: options.includeDomains,
-      exclude_domains: options.excludeDomains
+      exclude_domains: options.excludeDomains,
     });
 
-    return response.data;
+    return {
+      query,
+      responseTime: response.data.response_time,
+      images: response.data.images.map((image: any) => {
+        return {
+          url: image.url ? image.url : image,
+          description: image.description,
+        };
+      }),
+      results: response.data.results.map((result: any) => {
+        return {
+          title: result.title,
+          url: result.url,
+          content: result.content,
+          rawContent: result.raw_content,
+          score: result.score,
+          publishedDate: result.published_date,
+        }
+      }),
+      answer: response.data.answer,
+    };
   };
 }
--- src/types.ts
+++ src/types.ts
@@ -13,8 +13,31 @@ export type SearchOptions = {
   days?: number;
   maxResults?: number;
   includeImages?: boolean;
+  includeImageDescriptions?: boolean;
   includeAnswer?: boolean;
   includeRawContent?: boolean;
   includeDomains?: undefined | Array<string>;
   excludeDomains?: undefined | Array<string>;
 };
+
+type TavilyImage = {
+  url: string;
+  description?: string;
+};
+
+type TavilyResult = {
+  title: string;
+  url: string;
+  content: string;
+  rawContent?: string;
+  score: number;
+  publishedDate: string;
+};
+
+export type TavilyResponse = {
+  answer?: string;
+  query: string;
+  responseTime: number;
+  images: Array<TavilyImage>;
+  results: Array<TavilyResult>;
+}
